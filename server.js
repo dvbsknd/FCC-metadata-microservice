ad95cf62bcfc27e8eafeec41d4a0be3b892e2b4a
Dave Baskind <dave@picketstudio.com>
2020-08-19 19:19:49 +1000

    Experiment with reading streams

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const gitWebhook = require('./webhooks/git.js');
 app.use('/git', gitWebhook);
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -17,6 +18,17 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', (req, res) => {
+  console.log(req.body);
+  const chunks = [];
+  req.on('data', chunk => chunks.push(chunk));
+  req.on('end', () => {
+    const data = Buffer.concat(chunks);
+    console.log(data);
+    res.send(data)
+  });
 });
 
 const listener = app.listen(process.env.PORT, () => {
